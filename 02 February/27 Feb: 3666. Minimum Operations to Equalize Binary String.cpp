5ee5f9ecb732e38b55b6d0d70c79f993d133241e
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2026-02-27 17:47:35 +0530

    Update 27 Feb: 3666. Minimum Operations to Equalize Binary String.cpp

--- 02 February/27 Feb: 3666. Minimum Operations to Equalize Binary String.cpp
+++ 02 February/27 Feb: 3666. Minimum Operations to Equalize Binary String.cpp
@@ -1,55 +1,34 @@
-public class Solution {
-    public int MinOperations(string s, int k) {
-        int n = s.Length;
-
-        var ts = new SortedSet<int>[2];
-        ts[0] = new SortedSet<int>();
-        ts[1] = new SortedSet<int>();
-
+class Solution {
+public:
+    int minOperations(string s, int k) {
+        int n = s.size(), m = 0;
+        vector<int> dist(n + 1, INT_MAX);
+        vector<set<int>> nodeSets(2);
         for (int i = 0; i <= n; i++) {
-            ts[i % 2].Add(i);
-        }
-
-        int cnt0 = 0;
-        foreach (char c in s) {
-            if (c == '0') {
-                cnt0++;
+            nodeSets[i % 2].insert(i);
+            if (i < n && s[i] == '0') {
+                m++;
             }
         }
-
-        ts[cnt0 % 2].Remove(cnt0);
-
-        var q = new Queue<int>();
-        q.Enqueue(cnt0);
-
-        int ans = 0;
-
-        while (q.Count > 0) {
-            int size = q.Count;
-            for (int i = 0; i < size; i++) {
-                int cur = q.Dequeue();
-                if (cur == 0) {
-                    return ans;
-                }
-
-                int l = cur + k - 2 * Math.Min(cur, k);
-                int r = cur + k - 2 * Math.Max(k - n + cur, 0);
-
-                var t = ts[l % 2];
-
-                var toRemove = new List<int>();
-                foreach (int next in t.GetViewBetween(l, r)) {
-                    q.Enqueue(next);
-                    toRemove.Add(next);
-                }
-
-                foreach (int next in toRemove) {
-                    t.Remove(next);
-                }
+        queue<int> q;
+        q.push(m);
+        dist[m] = 0;
+        nodeSets[m % 2].erase(m);
+        while (!q.empty()) {
+            m = q.front();
+            q.pop();
+            int c1 = max(k - n + m, 0), c2 = min(m, k);
+            int lnode = m + k - 2 * c2, rnode = m + k - 2 * c1;
+            auto& nodeSet = nodeSets[lnode % 2];
+            for (auto iter = nodeSet.lower_bound(lnode);
+                 iter != nodeSet.end() && *iter <= rnode;) {
+                int m2 = *iter;
+                dist[m2] = dist[m] + 1;
+                q.push(m2);
+                iter = next(iter);
+                nodeSet.erase(m2);
             }
-            ans++;
         }
-
-        return -1;
+        return dist[0] == INT_MAX ? -1 : dist[0];
     }
-}
+};
