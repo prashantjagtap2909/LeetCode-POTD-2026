e8afb8882dc6330d67938441039df8c3b5acca76
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2026-03-15 22:13:44 +0530

    Update 15 Mar: 1622. Fancy Sequence.cpp

--- 03 March/15 Mar: 1622. Fancy Sequence.cpp
+++ 03 March/15 Mar: 1622. Fancy Sequence.cpp
@@ -1,31 +1,44 @@
-//translated using AI
 class Fancy {
 public:
-    vector<long long> arr;
-    long long MOD = 1000000007LL;
+    const long long MOD = 1e9 + 7;
+    vector<long long> nums;
+    long long mul = 1;
+    long long add = 0;
 
-    Fancy() {
-        
+    long long modPow(long long a, long long b) {
+        long long res = 1;
+        while (b) {
+            if (b & 1) res = res * a % MOD;
+            a = a * a % MOD;
+            b >>= 1;
+        }
+        return res;
     }
-    
+
+    long long modInv(long long x) {
+        return modPow(x, MOD - 2);
+    }
+
+    Fancy() {}
+
     void append(int val) {
-        arr.push_back(val);
+        long long v = (val - add) % MOD;
+        if (v < 0) v += MOD;
+        v = v * modInv(mul) % MOD;
+        nums.push_back(v);
     }
-    
+
     void addAll(int inc) {
-        for (int i = 0; i < arr.size(); i++) {
-            arr[i] = (arr[i] + inc) % MOD;
-        }
+        add = (add + inc) % MOD;
     }
-    
+
     void multAll(int m) {
-        for (int i = 0; i < arr.size(); i++) {
-            arr[i] = (arr[i] * m) % MOD;
-        }
+        mul = mul * m % MOD;
+        add = add * m % MOD;
     }
-    
+
     int getIndex(int idx) {
-        if (idx >= arr.size()) return -1;
-        return arr[idx] % MOD;
+        if (idx >= nums.size()) return -1;
+        return (nums[idx] * mul + add) % MOD;
     }
 };
