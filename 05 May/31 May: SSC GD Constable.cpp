d059541a52e71485948367962fc897c98d7034b1
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2026-05-31 17:11:48 +0530

    Create 31 May: SSC GD Constable.cpp

---
+++ 05 May/31 May: SSC GD Constable.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
+        unsigned xmax=0, xmin=1e5;
+        for(unsigned x: asteroids){
+            freq[x]++;
+            xmin=min(xmin, x);
+            xmax=max(xmax, x);
+        }
+        long long planet=mass;// careful for overflow
+        for(int x=xmin; x<=xmax; x++){
+            if (freq[x]==0) continue;
+            if (x>planet) {
+                memset(freq+x, 0, (xmax-x+1)*sizeof(unsigned));
+                return 0;
+            }
+            planet+=(long long)x*freq[x];
+            freq[x]=0;
+        }
+        return 1;
+    }
+};
