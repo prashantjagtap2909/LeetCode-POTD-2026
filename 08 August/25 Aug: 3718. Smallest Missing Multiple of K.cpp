5c2d7469f5c09033d2f2b5550877b47744c53547
Prashant Jagtap <93985255+prashantjagtap2909@users.noreply.github.com>
2026-08-25 22:58:48 +0530

    Update 25 Aug: 3718. Smallest Missing Multiple of K.cpp

--- 08 August/25 Aug: 3718. Smallest Missing Multiple of K.cpp
+++ 08 August/25 Aug: 3718. Smallest Missing Multiple of K.cpp
@@ -1,12 +1,19 @@
 class Solution {
 public:
     int missingMultiple(vector<int>& nums, int k) {
-        bitset<101> hasQ=1;
+        bitset<101> hasQ=1;// set 0th bit on
         int qMax=0;
         for(int x: nums){
             auto [q, r]=div(x, k);
             if (r==0){
                 hasQ[q]=1;
                 qMax=max(q, qMax);
+            }
+        }
+        auto A=bit_cast<array<uint64_t, 2>>(hasQ);
+        int q0=countr_one(A[0]); 
+        if (q0<64) return q0*k;
+        int q1=countr_one(A[1]);
+        return (q1+64)*k;
     }
 };
